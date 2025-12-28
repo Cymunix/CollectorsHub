00a614c5b57f37853c0efabea099b320605d3d19
Cymunix <hayden.macivor@gmail.com>
2025-12-28 12:04:09 -0400

    Update page.tsx

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
+import React, { useState } from "react";
 import Header from "@/components/Header";
 import SecondaryNav from "@/components/SecondaryNav";
 import AuthModal from "@/components/AuthModal";
 import CollectionScreen from "./_components/CollectionScreen";
 
 export default function CollectionPage() {
+  const [authOpen, setAuthOpen] = useState(false);
+
   return (
     <>
       <Header />
       <SecondaryNav />
-      <AuthModal />
+
+      <AuthModal open={authOpen} onClose={() => setAuthOpen(false)} />
+
       <main className="mx-auto max-w-6xl px-4 pb-16 pt-6">
         <CollectionScreen />
       </main>
