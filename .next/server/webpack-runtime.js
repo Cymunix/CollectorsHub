9036296b92c6f1682d87b7feedf781b8604353a3
Cymunix <hayden.macivor@gmail.com>
2026-01-11 21:09:16 -0400

    Updated trace

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3832e9cb18c71463")
+/******/ 		__webpack_require__.h = () => ("38815628a200f56e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return undefined;
+/******/ 			return "static/chunks/" + chunkId + ".js";
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28a27610757cd558"; }
+/******/ 		__webpack_require__.h = function() { return "b9fca6166b7196a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
